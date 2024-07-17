8772244b0e44d3a4d61749f7d162b0ae889586c9
Glavyanov <dimitarglavianov@gmail.com>
2024-07-17 23:33:14 +0300

    added dropdown, avatar, icons

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,29 @@
 <template>
   <nav class="h-16 border-b bg-muted/40 flex justify-between px-6 items-center">
-    <form class="w-full max-w-96">
-      <input class="w-full pl-8 bg-background" type="text" placeholder="Search...">
+    <form class="relative w-full h-fit max-w-96">
+      <iconify-icon class="absolute top-[50%] translate-y-[-50%] left-2.5 text-muted-foreground" icon="lucide:search" width="18" height="18"></iconify-icon>
+      <Input
+        :modelValue="''"
+        class="w-full pl-8 bg-background"
+        type="text"
+        placeholder="Search..."
+      />
     </form>
-    <div class="w-8 h-8 rounded-full bg-white"></div>
+
+    <DropdownMenu>
+      <DropdownMenuTrigger>
+        <Avatar>
+          <AvatarImage src="https://avatars.githubusercontent.com/u/89041019?v=4" alt="@DGG" />
+          <AvatarFallback>DG</AvatarFallback>
+        </Avatar>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent>
+        <DropdownMenuLabel>My Account</DropdownMenuLabel>
+        <DropdownMenuSeparator />
+        <DropdownMenuItem>Profile</DropdownMenuItem>
+        <DropdownMenuItem>Subscription</DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
   </nav>
   <main>
     <RouterView />
@@ -11,5 +31,14 @@
 </template>
 
 <script setup lang="ts">
-
+import Input from '@/components/ui/input/Input.vue'
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger
+} from '@/components/ui/dropdown-menu'
+import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar'
 </script>
