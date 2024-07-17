e0208e3578de832f963dc8c4a29c4cc312f89c33
Glavyanov <dimitarglavianov@gmail.com>
2024-07-17 22:41:48 +0300

    added shadcn library for components and tailwind css

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,10 @@
 <template>
+  <nav class="h-16 border-b bg-muted/40 flex justify-between px-6 items-center">
+    <form class="w-full max-w-96">
+      <input class="w-full pl-8 bg-background" type="text" placeholder="Search...">
+    </form>
+    <div class="w-8 h-8 rounded-full bg-white"></div>
+  </nav>
   <main>
     <RouterView />
   </main>
