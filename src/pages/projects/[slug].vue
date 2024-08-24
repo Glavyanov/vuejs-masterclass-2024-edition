b0973c33b96596016487387404b1957915ed813a
Glavyanov <dimitarglavianov@gmail.com>
2024-08-24 08:05:08 +0300

    align projects/[slug] ui layout with course flow.

--- src/pages/projects/[slug].vue
+++ src/pages/projects/[slug].vue
@@ -1,10 +1,87 @@
 <template>
-  <div>
-    <h1>Project:   {{ project?.name }}</h1>
-    <h2>status:   {{ project?.status }}</h2>
-    <h3>collaborators:   {{ JSON.stringify(project?.collaborators) }}</h3>
-    <RouterLink :to="{ name: '/' }"> Go to Home</RouterLink>
-  </div>
+  <Table>
+    <TableRow>
+      <TableHead> Name </TableHead>
+      <TableCell> Lorem ipsum dolor sit amet. </TableCell>
+    </TableRow>
+    <TableRow>
+      <TableHead> Description </TableHead>
+      <TableCell>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad iure qui tempora ex nihil, ab
+        reprehenderit dolorem sunt veritatis perferendis? Repudiandae quis velit quasi ab natus quia
+        ratione voluptas deserunt labore sed distinctio nam fuga fugit vero voluptates placeat
+        aperiam, saepe excepturi eos harum consectetur doloremque perspiciatis nesciunt! Incidunt,
+        modi.
+      </TableCell>
+    </TableRow>
+    <TableRow>
+      <TableHead> Status </TableHead>
+      <TableCell>In progress</TableCell>
+    </TableRow>
+    <TableRow>
+      <TableHead> Collaborators </TableHead>
+      <TableCell>
+        <div class="flex">
+          <Avatar
+            class="-mr-4 border border-primary hover:scale-110 transition-transform"
+            v-for="n in 5"
+            :key="n"
+          >
+            <RouterLink class="w-full h-full flex items-center justify-center" to="">
+              <AvatarImage src="" alt="" />
+              <AvatarFallback> </AvatarFallback>
+            </RouterLink>
+          </Avatar>
+        </div>
+      </TableCell>
+    </TableRow>
+  </Table>
+
+  <section class="mt-10 flex flex-col md:flex-row gap-5 justify-between grow">
+    <div class="flex-1">
+      <h2>Tasks</h2>
+      <div class="table-container">
+        <Table>
+          <TableHeader>
+            <TableRow>
+              <TableHead> Name </TableHead>
+              <TableHead> Status </TableHead>
+              <TableHead> Due Date </TableHead>
+            </TableRow>
+          </TableHeader>
+          <TableBody>
+            <TableRow v-for="n in 5" :key="n">
+              <TableCell> Lorem ipsum dolor sit amet. </TableCell>
+              <TableCell> In progress </TableCell>
+              <TableCell> 22/08/2024 </TableCell>
+            </TableRow>
+          </TableBody>
+        </Table>
+      </div>
+    </div>
+    <div class="flex-1">
+      <h2>Documents</h2>
+      <div class="table-container">
+        <p class="text-muted-foreground text-sm font-semibold px-4 py-3">
+          This project doesn't have documents yet...
+        </p>
+        <!-- <Table>
+          <TableHeader>
+            <TableRow>
+              <TableHead> Name </TableHead>
+              <TableHead> Visibility </TableHead>
+            </TableRow>
+          </TableHeader>
+          <TableBody>
+            <TableRow>
+              <TableCell> Lorem ipsum dolor sit amet. </TableCell>
+              <TableCell> Private </TableCell>
+            </TableRow>
+          </TableBody>
+        </Table> -->
+      </div>
+    </div>
+  </section>
 </template>
 
 <script setup lang="ts">
@@ -14,8 +91,10 @@ import type { Project } from '@/utils/supaQueries'
 usePageStore().pageData.title = "Project"
 
 const project = ref<Project | null>(null)
-const route = useRoute();
-const slug = ("slug" in route.params) ? route.params.slug : "";
+const route = useRoute('/projects/[slug]');
+// const route = useRoute(); => alternative way
+// const slug = ("slug" in route.params) ? route.params.slug : ""; => alternative way
+const slug = route.params?.slug;
 
 const loadProject = async () => {
   const { data, error } = await projectQuery(slug);
@@ -25,3 +104,19 @@ const loadProject = async () => {
 
 await loadProject();
 </script>
+
+<style>
+th {
+  @apply w-[100px];
+}
+
+h2 {
+  @apply mb-4 text-lg font-semibold w-fit;
+}
+
+.table-container {
+  @apply overflow-hidden overflow-y-auto rounded-md bg-slate-900 h-80;
+}
+</style>
+
+
