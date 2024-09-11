b6977a96cc8344dfda6eb562698d4e6d35f68cd0
Glavyanov <dimitarglavianov@gmail.com>
2024-09-11 05:54:43 +0300

    Refactor register page to use Vue 3 composition API

--- src/pages/register.vue
+++ src/pages/register.vue
@@ -1,5 +1,3 @@
-<script setup lang="ts"></script>
-
 <template>
   <div
     class="mx-auto w-full flex justify-center items-center p-10 text-center -mt-10 min-h-[90vh] h-full"
@@ -17,31 +15,32 @@
         <form class="grid gap-4">
           <div class="grid gap-2">
             <Label id="username" class="text-left">Username</Label>
-            <Input id="username" type="text" placeholder="johndoe19" required />
+            <Input v-model="formData.username" id="username" type="text" placeholder="johndoe19" required />
           </div>
           <div class="flex flex-col sm:flex-row justify-between gap-4">
             <div class="grid gap-2">
               <Label id="first_name" class="text-left">First Name</Label>
-              <Input id="first_name" type="text" placeholder="John" required />
+              <Input v-model="formData.firstName" id="first_name" type="text" placeholder="John" required />
             </div>
             <div class="grid gap-2">
               <Label id="last_name" class="text-left">Last Name</Label>
-              <Input id="last_name" type="text" placeholder="Doe" required />
+              <Input v-model="formData.lastName" id="last_name" type="text" placeholder="Doe" required />
             </div>
           </div>
           <div class="grid gap-2">
             <Label id="email" class="text-left">Email</Label>
-            <Input id="email" type="email" placeholder="johndoe19@example.com" required />
+            <Input v-model="formData.email" id="email" type="email" placeholder="johndoe19@example.com" required />
           </div>
 
           <div class="grid gap-2">
             <Label id="password" class="text-left">Password</Label>
-            <Input id="password" type="password" placeholder="*****" autocomplete required />
+            <Input v-model="formData.password" id="password" type="password" placeholder="*****" autocomplete required />
           </div>
 
           <div class="grid gap-2">
             <Label id="confirm_password" class="text-left">Confirm Password</Label>
             <Input
+              v-model="formData.confirmPassword"
               id="confirm_password"
               type="password"
               placeholder="*****"
@@ -60,3 +59,14 @@
     </Card>
   </div>
 </template>
+
+<script setup lang="ts">
+const formData = ref({
+  username: '',
+  firstName: '',
+  lastName: '',
+  email: '',
+  password: '',
+  confirmPassword: ''
+})
+</script>
