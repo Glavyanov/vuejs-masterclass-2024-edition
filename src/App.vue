dda8c62bd932ee3e84d2be6c10b0c63351b0c836
Glavyanov <dimitarglavianov@gmail.com>
2024-09-28 20:45:14 +0300

    Retrieve the Auth Session using Supabase Client

--- src/App.vue
+++ src/App.vue
@@ -15,10 +15,20 @@
 </template>
 
 <script setup lang="ts">
+import { supabase } from './lib/supabaseClient';
+
 const errorStore = useErrorStore();
+const authStore = useAuthStore();
 
 onErrorCaptured((error) => {
   errorStore.setError({ error });
+});
+
+onMounted(async () => {
+  const { data, error } = await supabase.auth.getSession()
+  if(data.session?.user) {
+   await authStore.setAuth(data.session);
+  }
 });
 
 // mocking component
