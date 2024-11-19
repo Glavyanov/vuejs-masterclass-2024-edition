888164c530cfa6c98c326eadb9a33dbcc6380612
Glavyanov <dimitarglavianov@gmail.com>
2024-11-19 18:26:39 +0200

    Set Up Eslint 9 with Flat Config in Vue.js

--- src/stores/loaders/projects.ts
+++ src/stores/loaders/projects.ts
@@ -1,30 +1,30 @@
-import { projectsQuery, type Projects } from "@/utils/supaQueries";
-import { useMemoize } from "@vueuse/core";
+import { projectsQuery, type Projects } from '@/utils/supaQueries'
+import { useMemoize } from '@vueuse/core'
 
-export const useProjectsStore = defineStore("projects-store", () => {
-    const projects = ref<Projects>([]);
+export const useProjectsStore = defineStore('projects-store', () => {
+  const projects = ref<Projects>([])
 
-    const loadProjects = useMemoize(async (key: string) => await projectsQuery);
+  const loadProjects = useMemoize(async (key: string) => await projectsQuery)
 
-    const getProjects = async () => {
-        if(projects.value?.length){
-            projectsQuery.then(({ data }) => {
-               if(JSON.stringify(data) !== JSON.stringify(projects.value)) {
-                   loadProjects.delete('projects');
-                   if(!error && data) projects.value = data;
-               }
-            });
+  const getProjects = async () => {
+    if (projects.value?.length) {
+      projectsQuery.then(({ data }: { data: Projects[] }) => {
+        if (JSON.stringify(data) !== JSON.stringify(projects.value)) {
+          loadProjects.delete('projects')
+          if (!error && data) projects.value = data
         }
-
-        const { data, error, status } = await loadProjects('projects');
-        if(data) projects.value = [...data]
-        if (error){
-            useErrorStore().setError({error, customCode: status});
-        }
-    };
-    
-    return {
-        projects,
-        getProjects
+      })
     }
-});
+
+    const { data, error, status } = await loadProjects('projects')
+    if (data) projects.value = [...data]
+    if (error) {
+      useErrorStore().setError({ error, customCode: status })
+    }
+  }
+
+  return {
+    projects,
+    getProjects
+  }
+})
