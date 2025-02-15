391561183b26496a3fa70b0f2cce39c3c1efba41
Glavyanov <dimitarglavianov@gmail.com>
2025-02-15 16:15:52 +0200

    Install and Use FormKit in Vue.js 3

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,7 +10,8 @@ export default {
     './pages/**/*.{ts,tsx,vue}',
     './components/**/*.{ts,tsx,vue}',
     './app/**/*.{ts,tsx,vue}',
-    './src/**/*.{ts,tsx,vue}'
+    './src/**/*.{ts,tsx,vue}',
+    './formkit.theme.ts'
   ],
 
   theme: {
